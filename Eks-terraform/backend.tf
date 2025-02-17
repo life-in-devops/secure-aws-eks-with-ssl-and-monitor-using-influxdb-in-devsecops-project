5806cc2fb8c9760d57eb846fa6f0c1fbf5ec8caf
Narasimha Jetty <129029918+life-in-devops@users.noreply.github.com>
2025-02-17 16:57:08 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mrcloudbooktwotier" # Replace with your actual S3 bucket name
-    key    = "EKS-Ajay/terraform.tfstate"
+    bucket = "devops-preparation-outcome" # Replace with your actual S3 bucket name
+    key    = "secure-aws-eks-with-ssl-and-monitor-using-influxdb-in-devsecops-project/EKS-Cluster/terraform.tfstate"
     region = "ap-south-1"
   }
 }
